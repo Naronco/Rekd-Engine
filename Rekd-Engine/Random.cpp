ce313f665489300358f579e801946684703abc0f
WebFreak001 <janju007@web.de>
2014-03-09 14:45:53 +0100

    Added Random

---
+++ Rekd-Engine/Random.cpp
@@ -0,0 +1,47 @@
+#include "Random.h"
+
+Rekd2D::Core::Random::Random()
+{
+	x = 45614563 + GetTickCount64();
+	x = 89215656 + GetTickCount64();
+	x = 23452554 + GetTickCount64();
+	x = 16145861 + GetTickCount64();
+}
+Rekd2D::Core::Random::Random(int seed)
+{
+	if (seed < 0) seed = -seed;
+	if (seed == 0) seed++;
+	x = seed;
+	y = seed + 458;
+	z = seed + 4756;
+	w = seed + 2978;
+}
+Rekd2D::Core::Random::Random(unsigned int seed1, unsigned int seed2, unsigned int seed3, unsigned int seed4)
+{
+	if (seed1 == 0) seed1++;
+	if (seed2 == 0) seed2++;
+	if (seed3 == 0) seed3++;
+	if (seed4 == 0) seed4++;
+	x = seed1;
+	y = seed2;
+	z = seed3;
+	w = seed4;
+}
+unsigned int Rekd2D::Core::Random::Next()
+{
+	t = x ^ (x << 11);
+	x = y; y = z; z = w;
+	return w = w ^ (w >> 19) ^ (t ^ (t >> 8));
+}
+unsigned int Rekd2D::Core::Random::Next(unsigned int max)
+{
+	return Next() % max;
+}
+bool Rekd2D::Core::Random::NextBool()
+{
+	return Next(2) == 0;
+}
+float Rekd2D::Core::Random::NextFloat()
+{
+	return Next(25) / ((float)(1 << 24));
+}
---
+++ Rekd-Engine/Random.h
@@ -0,0 +1,26 @@
+#ifndef REKD_RANDOM_H_
+#define REKD_RANDOM_H_
+
+#include <Windows.h>
+
+namespace Rekd2D
+{
+	namespace Core
+	{
+		class Random
+		{
+		public:
+			Random();
+			Random(int seed);
+			Random(unsigned int seed1, unsigned int seed2, unsigned int seed3, unsigned int seed4);
+
+			unsigned int Next(unsigned int max);
+			unsigned int Next();
+			float NextFloat();
+			bool NextBool();
+		protected:
+			unsigned int x, y, z, w, t;
+		};
+	}
+}
+#endif
