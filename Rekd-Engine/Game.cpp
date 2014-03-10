e16925f34739946385577e32ae38f3424ac5ba65
WebFreak001 <janju007@web.de>
2014-03-10 17:33:16 +0100

    Added Mouse handling to Game

--- Rekd-Engine/Game.cpp
+++ Rekd-Engine/Game.cpp
@@ -12,6 +12,33 @@ void Rekd2D::Core::Game::Run(char* title, unsigned int width, unsigned int heigh
 	m_Window->Show();
 	while (m_Window->Update(&e))
 	{
+		if (e.Type == Enum::EventType::MouseMove)
+		{
+			MouseState s = Mouse::GetState();
+			s.X = (int)((e.MouseMove.DestinationX + e.MouseMove.RelativeX) / (float)m_Window->GetWidth() * width);
+			s.Y = (int)((e.MouseMove.DestinationY + e.MouseMove.RelativeY) / (float)m_Window->GetHeight() * height);
+			s.relX = e.MouseMove.RelativeX;
+			s.relY = e.MouseMove.RelativeY;
+			Mouse::SetState(s);
+		}
+		else if (e.Type == Enum::EventType::MouseClick)
+		{
+			MouseState s = Mouse::GetState();
+			s.X = (int)(e.MouseButton.X / (float)m_Window->GetWidth() * width);
+			s.Y = (int)(e.MouseButton.Y / (float)m_Window->GetHeight() * height);
+			if (e.MouseButton.Button < 4)
+				s.mouseButtons[e.MouseButton.Button] = true;
+			Mouse::SetState(s);
+		}
+		else if (e.Type == Enum::EventType::MouseRelease)
+		{
+			MouseState s = Mouse::GetState();
+			s.X = (int)(e.MouseButton.X / (float)m_Window->GetWidth() * width);
+			s.Y = (int)(e.MouseButton.Y / (float)m_Window->GetHeight() * height);
+			if (e.MouseButton.Button < 4)
+				s.mouseButtons[e.MouseButton.Button] = false;
+			Mouse::SetState(s);
+		}
 		Update();
 		Render();
 	}
