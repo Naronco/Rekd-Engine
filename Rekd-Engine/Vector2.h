6a0ce7c860a01f4c2531543eef0c710143ddfdae
WebFreak001 <janju007@web.de>
2014-03-17 21:33:34 +0100

    Added @NaroncoNexo 's Matrix & Vector classes

--- Rekd-Engine/Vector2.h
+++ Rekd-Engine/Vector2.h
@@ -2,178 +2,62 @@
 #define REKD_VECTOR2_H_
 
 #include <math.h>
-#include "Matrix3x3.h"
 
 namespace Rekd2D
 {
 	namespace Core
 	{
-		template <typename T> struct Vector2
+		template <typename T>
+		struct Vector2
 		{
-			T X, Y;
-
-			inline Vector2() : X(default(T)), Y(default(T))
+			union
 			{
-			}
+				struct
+				{
+					T x, y;
+				};
 
-			inline Vector2(T X_, T Y_) : X(X_), Y(Y_)
-			{
-			}
+				T xy[2];
+			};
 
-			inline Vector2(const Vector2<T>& v) : X(v.X), Y(v.Y)
-			{
-			}
+			inline Vector2(T _x = 0, T _y = 0) : x(_x), y(_y) {  }
+			inline Vector2(const Vector2<T> &v) : x(v.x), y(v.y) {  }
 
-			inline Vector2<T>& Clear()
-			{
-				X = Y = 0;
-				return *this;
-			}
+			inline Vector2<T> &Clear() { x = y = 0; return *this; }
+			inline Vector2<T> &Set(T _x, T _y) { x = _x; y = _y; return *this; }
+			inline Vector2<T> &Set(const Vector2<T> &v) { x = v.x; y = v.y; return *this; }
 
-			inline Vector2<T>& Set(T X_, T Y_)
-			{
-				X = X_;
-				Y = Y_;
-				return *this;
-			}
+			template <typename S> inline bool operator == (const Vector2<S> &v) const { return x == v.x && y == v.y; }
+			template <typename S> inline bool operator != (const Vector2<S> &v) const { return x != v.x || y != v.y; }
 
-			inline Vector2<T>& Set(const Vector2<T>& v)
-			{
-				X = v.X;
-				Y = v.Y;
-				return *this;
-			}
+			inline Vector2<T> operator - () const { return Vector2<T>(-x, -y); }
 
-			inline Vector2<T>& operator += (const Vector2<T>& v)
-			{
-				return Set(X + v.X, Y + v.Y);
-			}
+			template <typename S> inline Vector2<T> operator + (const Vector2<S> &v) const { return Vector2<T>(x + v.x, y + v.y); }
+			template <typename S> inline Vector2<T> operator - (const Vector2<S> &v) const { return Vector2<T>(x - v.x, y - v.y); }
+			template <typename S> inline Vector2<T> operator * (S val) const { return Vector2<T>(x * val, y * val); }
+			template <typename S> inline Vector2<T> operator / (S val) const { T ival = (T)(1.0 / val); return Vector2<T>(x * ival, y * ival); }
 
-			inline Vector2<T>& operator -= (const Vector2<T>& v)
-			{
-				return Set(X - v.X, Y - v.Y);
-			}
+			template <typename S> inline Vector2<T> &operator += (const Vector2<S> &v) { return Set(x + v.x, y + v.y); }
+			template <typename S> inline Vector2<T> &operator -= (const Vector2<S> &v) { return Set(x - v.x, y - v.y); }
+			template <typename S> inline Vector2<T> &operator *= (S val) { return Set(x * val, y * val); }
+			template <typename S> inline Vector2<T> &operator /= (S val) { T ival = (T)(1.0 / val); return Set(x * ival, y * ival); }
 
-			inline Vector2<T>& operator *= (T value)
-			{
-				return Set(X * value, Y * value);
-			}
+			inline T GetSquaredLength() const { return x * x + y * y; }
+			inline T GetLength() const { return (T)sqrt(x * x + y * y); }
 
-			inline Vector2<T>& operator /= (T value)
-			{
-				T invValue = 1.0 / value;
-				return Set(X * invValue, Y * invValue);
-			}
+			template <typename S> inline T GetSquaredDistance(const Vector2<S> &v) { T xd = (T)(v.x - x); T yd = (T)(v.y - y); return xd * xd + yd * yd; }
+			template <typename S> inline T GetDistance(const Vector2<S> &v) { T xd = (T)(v.x - x); T yd = (T)(v.y - y); return (T)sqrt(xd * xd + yd * yd); }
 
-			inline Vector2<T> operator + (const Vector2<T>& v) const
-			{
-				return Vector2<T>(X + v.X, Y + v.Y);
-			}
+			inline Vector2<T> &Normalize() { return (*this * (T)(1.0 / GetLength())); }
 
-			inline Vector2<T> operator - (const Vector2<T>& v) const
-			{
-				return Vector2<T>(X - v.X, Y - v.Y);
-			}
-
-			inline Vector2<T> operator * (T value) const
-			{
-				return Vector2<T>(X * value, Y * value);
-			}
-
-			inline Vector2<T> operator / (T value) const
-			{
-				T invValue = 1.0 / value;
-				return Vector2<T>(X * invValue, Y * invValue);
-			}
-
-			inline bool operator == (const Vector2<T>& v) const
-			{
-				return (X == v.X) && (Y == v.Y);
-			}
-
-			inline bool operator != (const Vector2<T>& v) const
-			{
-				return (X != v.X) || (Y != v.Y);
-			}
-
-			inline T Dot(const Vector2<T>& v) const
-			{
-				return (X * v.X) + (Y * v.Y);
-			}
-
-			inline T Cross(const Vector2<T>& v) const
-			{
-				return X * v.Y - Y * v.X;
-			}
-
-			inline T GetSquaredLength() const
-			{
-				return X * X + Y * Y;
-			}
-
-			inline T GetLength() const
-			{
-				return (T)sqrt(X * X + Y * Y);
-			}
-
-			inline T GetSquaredDistance(const Vector2<T>& v)
-			{
-				T xd = v.X - X;
-				T yd = v.Y - Y;
-				return xd * xd + yd * yd;
-			}
-
-			inline T GetDistance(const Vector2<T>& v)
-			{
-				T xd = v.X - X;
-				T yd = v.Y - Y;
-				return (T)sqrt(xd * xd + yd * yd);
-			}
-
-			template <typename S>
-			inline Vector2<T>& Lerp(const Vector2<T>& v, S t)
-			{
-				return (*this += (v - *this) * t);
-			}
-
-			inline Vector2<T>& Normalize()
-			{
-				return (*this *= (1.0 / GetLength()));
-			}
-
-			inline Vector2<T> GetNormalized() const
-			{
-				return (*this * (1.0 / GetLength()));
-			}
+			template <typename S> inline T Dot(const Vector2<S> &v) { return x * v.x + y * v.y; }
+			template <typename S> inline T Cross(const Vector2<S> &v) { return x * v.y - y * v.x; }
 		};
 
+		template <typename T, typename S> inline Vector2<T> operator * (S val, const Vector2<T> &v) { return Vector2<T>(v.x * val, v.y * val); }
+
 		typedef Vector2<float> Vector2F;
-		typedef Vector2<int> Vector2I;
 		typedef Vector2<double> Vector2D;
-
-		template <typename T>
-		inline Rekd2D::Core::Vector2<T> operator * (T value, const Rekd2D::Core::Vector2<T>& v)
-		{
-			return Vector2<T>(v.X * value, v.Y * value);
-		}
-
-		template <typename T1, typename T2>
-		inline Rekd2D::Core::Vector2<T1> operator * (const Rekd2D::Core::Vector2<T1>& v, const Rekd2D::Core::Matrix3x3<T2> &m)
-		{
-			return Vector2<T1>(v.X * m.m00 + v.Y * m.m01 + m.m02, v.X * m.m10 + v.Y * m.m11 + m.m12);
-		}
-
-		template <typename T1, typename T2>
-		inline Rekd2D::Core::Vector2<T1> operator * (const Rekd2D::Core::Matrix3x3<T2> &m, const Rekd2D::Core::Vector2<T1>& v)
-		{
-			return Vector2<T1>(v.X * m.m00 + v.Y * m.m01 + m.m02, v.X * m.m10 + v.Y * m.m11 + m.m12);
-		}
-
-		template <typename T1, typename T2>
-		inline Rekd2D::Core::Vector2<T1> operator *= (const Rekd2D::Core::Vector2<T1>& v, const Rekd2D::Core::Matrix3x3<T2> &m)
-		{
-			return v.Set(v.X * m.m00 + v.Y * m.m01 + m.m02, v.X * m.m10 + v.Y * m.m11 + m.m12);
-		}
 	}
 }
 
