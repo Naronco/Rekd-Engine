b5906cc749469f86ea8f94534db534b3c469569e
WebFreak001 <janju007@web.de>
2014-03-21 21:44:19 +0100

    Testing Textboxes

--- Rekd-Editor/MainWindow.cpp
+++ Rekd-Editor/MainWindow.cpp
@@ -7,6 +7,8 @@ MainWindow::MainWindow()
 void MainWindow::Load()
 {
 	AddCheckbox(32, 32);
+	AddTextbox(32, 70);
+	AddTextbox(32, 110);
 }
 
 void MainWindow::Deinit()
@@ -17,9 +19,18 @@ void MainWindow::AddCheckbox(int x, int y)
 {
 	CheckboxComponent* checkbox = new CheckboxComponent();
 	Matrix3x3F transform;
-	transform.Rotate(0.9f);
-	transform.Translate(100, 0);
+	transform.Translate(x, y);
 	transform.Scale(32, 32);
 	checkbox->m_Transform = transform;
 	AddComponent(checkbox);
+}
+
+void MainWindow::AddTextbox(int x, int y)
+{
+	TextboxComponent* textbox = new TextboxComponent();
+	Matrix3x3F transform;
+	transform.Translate(x, y);
+	transform.Scale(128, 32);
+	textbox->m_Transform = transform;
+	AddComponent(textbox);
 }
--- Rekd-Editor/MainWindow.h
+++ Rekd-Editor/MainWindow.h
@@ -12,4 +12,5 @@ public:
 	void Load();
 	void Deinit();
 	void AddCheckbox(int x, int y);
+	void AddTextbox(int x, int y);
 };
